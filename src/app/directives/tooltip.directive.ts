7845b449676be3ec9b43696b79e77bf1d3a99317
riyafadadu04 <riyafadadu2004@gmail.com>
2025-01-17 10:25:49 +0530

    tooltip without using renderer

--- src/app/directives/tooltip.directive.ts
+++ src/app/directives/tooltip.directive.ts
@@ -5,7 +5,6 @@ import {
   Input,
   OnChanges,
   OnInit,
-  Renderer2,
   SimpleChanges,
 } from '@angular/core';
 
@@ -22,7 +21,7 @@ export class TooltipDirective implements OnInit, OnChanges {
 
   private tooltip: HTMLElement | null = null;
 
-  constructor(private eleRef: ElementRef, private renderer: Renderer2) {}
+  constructor(private eleRef: ElementRef) {}
 
   ngOnInit() {
     this.createTooltip();
@@ -31,17 +30,13 @@ export class TooltipDirective implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (this.tooltip) {
       if (changes['tooltipBackgroundColor']) {
-        this.renderer.setStyle(
-          this.tooltip,
-          'background',
-          this.tooltipBackgroundColor
-        );
+        this.tooltip.style.backgroundColor = this.tooltipBackgroundColor;
       }
       if (changes['tooltipFontColor']) {
-        this.renderer.setStyle(this.tooltip, 'color', this.tooltipFontColor);
+        this.tooltip.style.color = this.tooltipFontColor;
       }
       if (changes['tooltipFontSize']) {
-        this.renderer.setStyle(this.tooltip, 'fontSize', this.tooltipFontSize);
+        this.tooltip.style.fontSize = this.tooltipFontSize;
       }
       if (changes['tooltipPosition']) {
         this.setPositionStyles();
@@ -50,53 +45,50 @@ export class TooltipDirective implements OnInit, OnChanges {
   }
 
   private createTooltip() {
-    this.tooltip = this.renderer.createElement('span');
-    this.tooltip!.innerText =
-      this.tooltipContent.length == 0
-        ? 'By default tooltip content'
-        : this.tooltipContent;
+    this.tooltip = document.createElement('span');
+    this.tooltip.innerText = this.tooltipContent || 'By default tooltip content';
 
-    this.renderer.setStyle(this.tooltip, 'position', 'absolute');
-    this.renderer.setStyle(
-      this.tooltip,
-      'background',
-      this.tooltipBackgroundColor
-    );
-    this.renderer.setStyle(this.tooltip, 'padding', '5px');
-    this.renderer.setStyle(this.tooltip, 'border', '1px solid #ccc');
-    this.renderer.setStyle(this.tooltip, 'borderRadius', '4px');
-    this.renderer.setStyle(this.tooltip, 'fontSize', this.tooltipFontSize);
-    this.renderer.setStyle(this.tooltip, 'color', this.tooltipFontColor);
-    this.renderer.setStyle(this.tooltip, 'display', 'none');
-    this.renderer.setStyle(this.tooltip, 'zIndex', '1000');
-    this.renderer.setStyle(this.tooltip, 'transform', 'translateX(-50%)');
+    Object.assign(this.tooltip.style, {
+      position: 'absolute',
+      backgroundColor: this.tooltipBackgroundColor,
+      padding: '5px',
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      fontSize: this.tooltipFontSize,
+      color: this.tooltipFontColor,
+      display: 'none',
+      zIndex: '1000',
+      transform: 'translateX(-50%)',
+      whiteSpace: 'nowrap',
+    });
+
     this.setPositionStyles();
-    this.renderer.appendChild(this.eleRef.nativeElement, this.tooltip);
+    this.eleRef.nativeElement.appendChild(this.tooltip);
   }
 
   private setPositionStyles() {
     if (this.tooltip) {
-      this.renderer.setStyle(this.tooltip, 'bottom', null);
-      this.renderer.setStyle(this.tooltip, 'top', null);
-      this.renderer.setStyle(this.tooltip, 'left', null);
-      this.renderer.setStyle(this.tooltip, 'right', null);
-      this.renderer.setStyle(this.tooltip, 'whiteSpace', 'nowrap');
+      this.tooltip.style.top = '';
+      this.tooltip.style.bottom = '';
+      this.tooltip.style.left = '';
+      this.tooltip.style.right = '';
 
       switch (this.tooltipPosition) {
         case 'top':
-          this.renderer.setStyle(this.tooltip, 'bottom', '100%');
-          this.renderer.setStyle(this.tooltip, 'left', '50%');
+          this.tooltip.style.bottom = '100%';
+          this.tooltip.style.left = '50%';
           break;
         case 'right':
-          this.renderer.setStyle(this.tooltip, 'top', '40%');
-          this.renderer.setStyle(this.tooltip, 'left', '65%');
+          this.tooltip.style.top = '40%';
+          this.tooltip.style.left = '65%';
           break;
         case 'bottom':
-          this.renderer.setStyle(this.tooltip, 'left', '50%');
+          this.tooltip.style.top = '100%';
+          this.tooltip.style.left = '50%';
           break;
         case 'left':
-          this.renderer.setStyle(this.tooltip, 'top', '40%');
-          this.renderer.setStyle(this.tooltip, 'right', '55%');
+          this.tooltip.style.top = '40%';
+          this.tooltip.style.right = '55%';
           break;
       }
     }
@@ -104,13 +96,13 @@ export class TooltipDirective implements OnInit, OnChanges {
 
   @HostListener('mouseenter') onMouseEnter() {
     if (this.tooltip) {
-      this.renderer.setStyle(this.tooltip, 'display', 'block');
+      this.tooltip.style.display = 'block';
     }
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     if (this.tooltip) {
-      this.renderer.setStyle(this.tooltip, 'display', 'none');
+      this.tooltip.style.display = 'none';
     }
   }
 }
